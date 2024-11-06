8ee80258da82216179c5025f10c30a4b891fbf02
sullaspqr <sulla.spqr78@gmail.com>
2024-11-06 08:40:18 +0100

    első kommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+export const App=()=> {
+  const [items, setItems] = useState([]);
+  const [tipus, setTipus] = useState('comment');
+
+  useEffect(() => {
+    fetch("https://localhost:7231/" + tipus)
+      .then((res) => (res.ok ? res.json() : []))
+      .then((tartalom) => setItems(tartalom));
+  }, [tipus]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row m-5 border p-5">
+        <FormKomponens setTipus={setTipus} />
+        <ListaKomponens elemek={items} />
+      </div>
     </div>
   );
 }
-
-export default App;
+const FormKomponens = ({ setTipus }) => (
+  <form
+  className="w-100"
+  onSubmit={(event) => {
+    event.preventDefault();
+    setTipus(event.target.contentType.value);
+  }}>
+    <select name="contentType"
+    className="form-control mb-2">
+      <option value="comment">Kommentek</option>
+      <option value="post">Posztok</option>
+    </select>
+    <button className="btn btn-primary" type="submit">Kattints!</button>
+</form>
+);
+const ListaKomponens = ({ elemek }) => (
+  <ul>
+    {elemek.map((elem, index) => (
+      <li key={index} className="list-group-item">{elem.id} - {elem.body}</li>
+    ))}
+  </ul>
+);
